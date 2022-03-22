1ead58ee453be5ee164a7097e36bc34c590716b7
TheAthleticCoder <msg.harshit@gmail.com>
2022-03-22 20:04:09 +0530

    model made

--- model1.py
+++ model1.py
@@ -12,8 +12,9 @@ model_dict = {}
 for i in list(input_data.index):
     model_dict[i] = list(input_data.loc[i])
 
-words = ['<UNK>','the','an']
-word_vectors = np.array([model_dict[w] for w in words])
+# words = ['old','little','work','wife','negative']
+words = ['big']
+# word_vectors = np.array([model_dict[w] for w in words])
 # print(word_vectors)
 
 #display top 10 closest words to 'camera'
@@ -29,6 +30,26 @@ def top_similar(word, model_dict, n=10):
 # topit = top_similar('camera', model_dict)
 # print(topit)
 
+# topit = top_similar('camera', model_dict)
+# print(topit)
+
+#cosine similaroty to find closest words
+def cosine_similarity(vec1, vec2):
+    return np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+    
+
+def get_similar_words(words, model_dict, n=10):
+    total_word_list = []
+    for word in words:
+        similar_list = top_similar(word, model_dict, n)
+        for w in similar_list:
+            total_word_list.append(w[0])
+    total_word_vectors = np.array([model_dict[w] for w in total_word_list])
+    return total_word_list, total_word_vectors
+
+words, word_vectors = get_similar_words(words, model_dict, n=10)
+print("words:", words)
+
 #Display the top-10 word vectors for five different words using t-SNE (or such methods) on a 2D plot.
 def tsne_plot(word_vectors, words):
     tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
@@ -39,10 +60,15 @@ def tsne_plot(word_vectors, words):
         x.append(value[0])
         y.append(value[1])
     plt.figure(figsize=(16, 16))
-    for i, word in enumerate(words):
-        plt.scatter(x[i], y[i])
-        plt.annotate(word, xy=(x[i], y[i]), xytext=(5, 2), textcoords='offset points', ha='right', va='bottom')
+    #plot using sns
+    sns.scatterplot(x, y)
+    # for i, word in enumerate(words):
+    #     plt.scatter(x[i], y[i])
+    #     plt.annotate(word, xy=(x[i], y[i]), xytext=(5, 2), textcoords='offset points', ha='right', va='bottom')
     plt.show()
+
+
+
 
 tsne_plot(word_vectors, words)
 
--- word_vec.py
+++ word_vec.py
@@ -1,12 +1,43 @@
 import numpy as np
 import pandas as pd
 from gensim.models import Word2Vec
+from sklearn.manifold import TSNE
+import seaborn as sns
+import matplotlib.pyplot as plt
 import random
 
 #open txt file
 data_file = open('derived_data.txt', 'r')
 total_sentences = data_file.read().splitlines() #split lines
 data_file.close()
+
+#create a dictionary of all vocabulary words and their counts
+temp_word_counts = {}
+for sentence in total_sentences:
+    for word in sentence.split():
+        if word not in temp_word_counts:
+            temp_word_counts[word] = 1
+        else:
+            temp_word_counts[word] += 1
+    
+#for word_counts less than 3, remove the word and its count and add 'UNK' and its count
+word_counts = {}
+unk_counter = 0
+for word in temp_word_counts:
+    if temp_word_counts[word] >= 3:
+        word_counts[word] = temp_word_counts[word]
+    else:
+        unk_counter += temp_word_counts[word]
+word_counts['<UNK>'] = unk_counter
+
+#change sentences based on the presence of <UNK> words
+temp = []
+for sentence in total_sentences:
+    for word in sentence.split():
+        if word not in word_counts:
+            sentence = sentence.replace(word, '<UNK>')        
+    temp.append(sentence)
+total_sentences = temp
 
 vocab=list(set((" ".join(total_sentences)).split()))
 words_in_sentence = []
@@ -22,3 +53,36 @@ print(word_vectors.most_similar("camera"))
 # print(random.choice(model.wv.index_to_key))
 
 model.save("word2vec.model")
+
+# def tsne_plot(model):
+#     "Creates and TSNE model and plots it"
+#     labels = []
+#     tokens = []
+
+#     new_values = tsne_model.fit_transform(model.wv.get_vector_representation())
+
+#     for word in list(model.wv.index_to_key):
+#         tokens.append(model.wv.get_index(word))
+#         labels.append(word)
+    
+#     tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
+#     new_values = tsne_model.fit_transform(tokens)
+
+#     x = []
+#     y = []
+#     for value in new_values:
+#         x.append(value[0])
+#         y.append(value[1])
+        
+#     plt.figure(figsize=(16, 16)) 
+#     for i in range(len(x)):
+#         plt.scatter(x[i],y[i])
+#         plt.annotate(labels[i],
+#                      xy=(x[i], y[i]),
+#                      xytext=(5, 2),
+#                      textcoords='offset points',
+#                      ha='right',
+#                      va='bottom')
+#     plt.show()
+
+# tsne_plot(model)
